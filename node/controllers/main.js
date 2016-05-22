c2f30fed5d8c146ec60a04d61926073586ed832a
Anton Karmazin <karmazzin@ya.ru>
2016-05-22 03:20:57 +0300

    add empty controllers

--- node/controllers/main.js
+++ node/controllers/main.js
@@ -3,5 +3,17 @@
 module.exports = {
     home(req, res) {
         res.render('pages/home', { hello: 1, title: 'Панель управления'});
+    },
+    sensors(req, res) {
+        res.render('pages/home', { hello: 1, title: 'Датчики'});
+    },
+    graphics(req, res) {
+        res.render('pages/home', { hello: 1, title: 'Графики'});
+    },
+    schedule(req, res) {
+        res.render('pages/home', { hello: 1, title: 'Расписания'});
+    },
+    devices(req, res) {
+        res.render('pages/home', { hello: 1, title: 'Устройства'});
     }
 };
